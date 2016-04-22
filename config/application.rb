9dcb53879773c9afadebe875a0052202c81f7433
Bryce Lively <bryce.lively@gmail.com>
2016-04-22 17:24:39 -0400

    removed whiny reqs

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 HOSTNAME = ENV['HOSTNAME']
 module Tunr
   class Application < Rails::Application
-    config.web_console.whitelisted_ips = '10.0.2.2'
+    config.web_console.whiny_requests = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
