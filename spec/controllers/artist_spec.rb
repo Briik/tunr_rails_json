11aacc4ab726d6e2ad08f2632f600cb25e96db45
jen pengelly <jgpengelly@gmail.com>
2016-04-26 12:42:04 -0400

    update artist spec controller

--- spec/controllers/artist_spec.rb
+++ spec/controllers/artist_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ArtistsController, :type => :controller do
   describe "GET #index" do
-    it 'saves artists as instance variables' do
+    it 'loads all of the artists into @artists' do
       # Arrange
-      artist = create(:artist)
-      artist2 = create(:artist2)
+      artist1, artist2 = Artist.create!, Artist.create!
       # Act
       get :index
       # Assert
-      assigns(:artists).should eq [artist, artist2]
+      expect(assigns(:artists)).to match_array[artist, artist2]
     end
   end
 end
